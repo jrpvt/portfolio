db7937f7e5149aeaa453a1bb8b9b27834e611dc0
jrpvt <jrpowell@gmail.com>
2017-09-30 20:34:40 -0400

    Round 2

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,26 +4,17 @@ import StringIO
 import zipfile
 import mimetypes
 
-def lambda_handler(event, context):
-    # TODO implement
-    sns = boto3.resource('sns')
-    topic = sns.Topic('arn:aws:sns:us-east-1:447169797035:deployPortfolioTopic')
-    try:
-        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-        portfolio_bucket = s3.Bucket('portfolio.johnpowell.name')
-        build_bucket = s3.Bucket('portfoliobuild.johnpowell.name')
-        portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-        with zipfile.ZipFile(portfolio_zip) as myzip:
-            for nm in myzip.namelist():
-                obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm,
-                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-        print "Completed successfully!"
-        topic.publish(Subject="Code update", Message="Lambda pushed a code update to your portfolio!")
-    except:
-        topic.publish(Subject="Code update failed", Message="Lambda failed trying to push a code update to your portfolio...")
-        raise
-    return 'Hello from Lambda'
+portfolio_bucket = s3.Bucket('portfolio.johnpowell.name')
+build_bucket = s3.Bucket('portfoliobuild.johnpowell.name')
+
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj,nm,
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
