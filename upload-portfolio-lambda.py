19ba94b16b119af5c08a5a1ac6024486e38d66ae
jrpvt <jrpowell@gmail.com>
2017-09-30 21:51:24 -0400

    CodePipeline working
    Updated to work with CodePipeline

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,16 +5,28 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    # TODO implement
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:447169797035:deployPortfolioTopic')
+
+    location = {
+        "bucketName": 'portfoliobuild.johnpowell.name',
+        "objectKey": 'portfoliobuild.zip'
+    }
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+
+        print "Building portfolio from " + str(location)
         s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-        sns = boto3.resource('sns')
-        topic = sns.Topic('arn:aws:sns:us-east-1:447169797035:deployPortfolioTopic')
         portfolio_bucket = s3.Bucket('portfolio.johnpowell.name')
-        build_bucket = s3.Bucket('portfoliobuild.johnpowell.name')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -23,6 +35,9 @@ def lambda_handler(event, context):
                     ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
         topic.publish(Subject="Code update successful", Message="Code update successful")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
         topic.publish(Subject="Code update failed", Message="Code update failed")
         raise
